dbafa121e8ed1700fbe51e172d3407c40ff2415b
duskobogdanovski <dule.bogdanovski@gmail.com>
2021-03-19 12:28:21 +0100

    Remove licenses search facet for dataset, organization and group pages

--- ckanext/radius/plugin.py
+++ ckanext/radius/plugin.py
@@ -4,6 +4,7 @@ import ckan.plugins.toolkit as toolkit
 
 class RadiusPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets, inherit=True)
 
     # IConfigurer
 
@@ -13,3 +14,20 @@ class RadiusPlugin(plugins.SingletonPlugin):
         toolkit.add_resource('fanstatic',
             'radius')
         toolkit.add_resource('assets', 'radius_assets')
+
+    # IFacets
+
+    # Removing licenses facet from dataset search page
+    def dataset_facets(self, facets_dict, package_type):
+        del facets_dict[u'license_id']
+        return facets_dict
+
+    # Removing licenses facet from organization page
+    def group_facets(self, facets_dict, package_type):
+        del facets_dict[u'license_id']
+        return facets_dict
+
+    # Removing licenses facet from group page
+    def organization_facets(self, facets_dict, package_type):
+        del facets_dict[u'license_id']
+        return facets_dict
